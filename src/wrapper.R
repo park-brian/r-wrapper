f9324b5bfe2a1f7330aef6e6501e16916e5ae633
Brian Park <park-brian@users.noreply.github.com>
2021-05-18 16:09:53 -0400

    simplify arrays of objects as data frames

--- src/wrapper.R
+++ src/wrapper.R
@@ -7,6 +7,6 @@ outputFile <- args[4]
 source(sourceFile)
 functionArguments <- list()
 if (file.exists(functionArgumentsFile))
-    functionArguments <- jsonlite::read_json(functionArgumentsFile)
+    functionArguments <- jsonlite::read_json(functionArgumentsFile, simplifyDataFrame = T)
 output <- do.call(functionName, as.list(functionArguments))
 jsonlite::write_json(output, outputFile, auto_unbox = TRUE, force = TRUE, na = "null")
